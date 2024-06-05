742ebc6f6773bb19750a77395421a9af744aeb02
Lim Wen Hao <wenhaojshs@gmail.com>
2024-06-05 22:00:49 +0800

    First commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// Run the following command to compile the code:
+// g++ *.cpp -o main && ./main
+
+#include <iostream>
+using namespace std;
+
+int main () {
+  cout << "Hello, World!" << endl;
+  return 0;
+}
