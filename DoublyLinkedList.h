85489182e49d1a70ccc5e765b02ee88f16a92c6d
Lim Wen Hao <wenhaojshs@gmail.com>
2024-06-11 22:13:56 +0800

    Doc: header files

--- DoublyLinkedList.h
+++ DoublyLinkedList.h
@@ -1,47 +1,139 @@
 #ifndef DOUBLY_LINKED_LIST_CPP
 #define DOUBLY_LINKED_LIST_CPP
 
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <cmath>
+#include <iostream> // for input / output stream
+#include <iomanip> // for input / output manipulation
+#include <string> // for string manipulation
+#include <cmath> // for math operations
 
 using namespace std;
 
+/**
+ * @file DoublyLinkedList.h
+ * @brief This file contains the declaration of the DoublyLinkedList class.
+ */
+
+/**
+ * @class DoublyLinkedList
+ * @brief A class that represents a doubly linked list.
+ * @tparam T1 The type of the question in each node.
+ * @tparam T2 The type of the data in each node.
+ */
 template <class T1, class T2>
 class DoublyLinkedList {
   private:
+    /**
+     * @struct Node
+     * @brief A struct that represents a node in the doubly linked list.
+     */
     struct Node {
-      T1 question;
-      T2 data;
-      Node* next;
-      Node* prev;
+      T1 question; ///< The question stored in the node.
+      T2 data; ///< The data stored in the node.
+      Node* next; ///< Pointer to the next node.
+      Node* prev; ///< Pointer to the previous node.
     };
-    Node* head;
-    Node* tail;
+    Node* head; ///< Pointer to the head of the list.
+    Node* tail; ///< Pointer to the tail of the list.
   public:
+    /**
+     * @brief Constructs an empty DoublyLinkedList object.
+     */
     DoublyLinkedList();
 
+    /**
+     * @brief Edits the data of a node with the specified question.
+     * @param question The question of the node to be edited.
+     * @param data The new data to be assigned to the node.
+     */
     void editNode(T1 question, T2 data);
+
+    /**
+     * @brief Searches for a node with the specified question.
+     * @param question The question to search for.
+     * @return The index of the node if found, -1 otherwise.
+     */
     int searchQuestionNode(T1 question) const;
+
+    /**
+     * @brief Searches for a node with the specified data.
+     * @param data The data to search for.
+     * @return The index of the node if found, -1 otherwise.
+     */
     int searchNode(T2 data) const;
 
+    /**
+     * @brief Deletes the node with the specified question.
+     * @param question The question of the node to be deleted.
+     */
     void deleteQuestionNode(T1 question);
+
+    /**
+     * @brief Deletes the node with the specified data.
+     * @param data The data of the node to be deleted.
+     */
     void deleteNode(T2 data);
 
+    /**
+     * @brief Destroys the DoublyLinkedList object and frees the memory.
+     */
     ~DoublyLinkedList();
 
+    /**
+     * @brief Inserts a new node with the specified question and data.
+     * @param question The question of the new node.
+     * @param data The data of the new node.
+     */
     void insertNode(T1 question, T2 data);
     
+    /**
+     * @brief Displays the list of questions.
+     */
     void displayQuestionList() const;
+
+    /**
+     * @brief Displays the data of a node with the specified question.
+     * @param question The question of the node to display.
+     * @param field The field of the node to display.
+     */
     void displayNode(T1 question, string field) const;
 
+    /**
+     * @brief Gets the number of nodes in the list.
+     * @return The number of nodes in the list.
+     */
     int getNumberOfNodes() const;
 
-    T2 getHeadValue () const;
-    T2 getTailValue () const;
+    /**
+     * @brief Gets the value of the head node.
+     * @return The value of the head node.
+     */
+    T2 getHeadValue() const;
+
+    /**
+     * @brief Gets the value of the tail node.
+     * @return The value of the tail node.
+     */
+    T2 getTailValue() const;
+
+    /**
+     * @brief Gets the value of a node with the specified question.
+     * @param question The question of the node to get the value.
+     * @return The value of the node.
+     */
     T2 getNodeValue(T1 question) const;
+
+    /**
+     * @brief Gets the value of the previous node of a node with the specified question.
+     * @param question The question of the node to get the previous node value.
+     * @return The value of the previous node.
+     */
     T2 getPreviousNodeValue(T1 question) const;
+
+    /**
+     * @brief Gets the value of the next node of a node with the specified question.
+     * @param question The question of the node to get the next node value.
+     * @return The value of the next node.
+     */
     T2 getNextNodeValue(T1 question) const;
 };
 
--- Score.h
+++ Score.h
@@ -1,32 +1,72 @@
 #ifndef SCORE_H
 #define SCORE_H
 
-#include <iostream>
-#include <string>
-#include <iomanip>
+#include <iostream> // for input / output stream
+#include <string> // for string manipulation
+#include <iomanip> // for input / output manipulation
 using namespace std;
 
-#include "FlashCard.h"
+#include "FlashCard.h" // preprocessor directive to include the FlashCard header file
 
+/**
+ * @file Score.h
+ * @brief This file contains the declaration of the Score class.
+ */
+
+/**
+ * @class Score
+ * @brief A class that represents the score of a flashcard game.
+ * @tparam T1 The type of the flashcard question.
+ * @tparam T2 The type of the flashcard answer.
+ * @tparam T3 The type of the flashcard category.
+ * @tparam T4 The type of the user's answer.
+ * @tparam T5 The type of the flashcard difficulty.
+ */
 template <class T1, class T2, class T3, class T4, class T5>
 class Score : public FlashCard<T1, T2, T3, T4, T5> {
   private:
-    string name;
-    int age;
+    string name; /**< The name of the player. */
+    int age; /**< The age of the player. */
 
-    int correct;
-    int incorrect;
-    int total;
-    double percentage;
+    int correct; /**< The number of correct answers. */
+    int incorrect; /**< The number of incorrect answers. */
+    int total; /**< The total number of questions. */
+    double percentage; /**< The percentage of correct answers. */
   public:
+    /**
+     * @brief Default constructor for the Score class.
+     */
     Score();
 
+    /**
+     * @brief Updates the score based on the user's answer.
+     * @param question The flashcard question.
+     * @param userAnswer The user's answer.
+     */
     void updateScore(T1 question, T4 userAnswer);
+
+    /**
+     * @brief Displays the score.
+     */
     void displayScore() const;
+
+    /**
+     * @brief Resets the score to zero.
+     */
     void resetScore();
 
     // Mutators
+
+    /**
+     * @brief Sets the name of the player.
+     * @param name The name of the player.
+     */
     void setName(string name);
+
+    /**
+     * @brief Sets the age of the player.
+     * @param age The age of the player.
+     */
     void setAge(int age);
 };
 
